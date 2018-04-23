ece8a9ef7e579e105e71dbc9fb24234a8e382482
Zac Willmington <zac.willmington@gmail.com>
2018-04-23 23:12:11 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,7 +53,7 @@ class InteractiveRecord
 
   def self.find_by_name(name)
         sql = <<-SQL
-            SELECT * FROM #{self.table_name_for_insert} WHERE name = ?;
+            SELECT * FROM students WHERE name = ?;
         SQL
         binding.pry
         DB[:conn].execute(sql, name)
